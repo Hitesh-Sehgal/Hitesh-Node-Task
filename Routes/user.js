2732dbbc5413d014967f0389007e03651c287b8a
Hitesh <hitesh.sehgal@techugo.com>
2022-06-07 00:41:53 +0530

    Code Uploaded

---
+++ Routes/user.js
@@ -0,0 +1,29 @@
+let express = require('express'),
+    router = express.Router(),
+    userService = require('../Services/userService');
+authHandler = require('../middleware/verifyToken');
+
+
+/* User Signup */
+router.post('/userSignup', (req, res) => {
+    userService.signup(req.body, (data) => {
+        res.send(data);
+    });
+});
+
+/* User Login */
+router.post('/userLogin', (req, res) => {
+    userService.login(req.body, (data) => {
+        res.send(data);
+    });
+});
+
+/* Update Profile */
+router.post('/updateProfile', authHandler.verifyToken, (req, res) => {
+    userService.updateProfile(req.body, req.headers, (data) => {
+        res.send(data);
+    });
+});
+
+
+module.exports = router;
